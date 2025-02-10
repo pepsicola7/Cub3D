5cf2bb440512b6ff8c9831f72358cd323452e3a7
lpolizzi <poli.loulou@gmail.com>
2025-02-10 17:40:52 +0100

    Started project. Created some basic files and made a Makefile.

---
+++ includes/cub3d.h
@@ -0,0 +1,12 @@
+#ifndef CUB3D_H
+# define CUB3D_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <math.h>
+# include <string.h>
+# include "mlx.h"
+# include "libft.h"
+
+#endif

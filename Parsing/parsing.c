3bce6f62f9569e4498bbb0c725d0758bebf9745f
Prprkitty <168284039+pepsicola7@users.noreply.github.com>
2025-02-17 16:18:14 +0100

    ?

--- Parsing/parsing.c
+++ Parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 14:43:10 by peli              #+#    #+#             */
-/*   Updated: 2025/02/14 16:36:38 by peli             ###   ########.fr       */
+/*   Updated: 2025/02/17 16:12:51 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,41 +19,51 @@ int	get_texture(char *line, t_data *data)
 
 	path = ft_strchr(line, ' ');
 	if (!path)
-		return ("There have no name of texture");
+	{
+		printf ("There have no name of texture");
+		return (0);
+	}
 	texture = mlx_load_png(path);
 	if (!texture)
 		return (printf("The texture isn't exist"), 0);
-	if (ft_strncmp("NO ", 3))
-		data->texture_data->north = path;
-	if (ft_strncmp("SO ", 3))
-		data->texture_data->south = path;
-	if (ft_strncmp("WE ", 3))
-		data->texture_data->west = path;
-	if (ft_strncmp("EA ", 3))
-		data->texture_data->east = path;
-	return (0);
+	if (ft_strncmp(line, "NO ", 3))
+		data->texture_data->north = texture;
+	if (ft_strncmp(line, "SO ", 3))
+		data->texture_data->south = texture;
+	if (ft_strncmp(line, "WE ", 3))
+		data->texture_data->west = texture;
+	if (ft_strncmp(line, "EA ", 3))
+		data->texture_data->east = texture;
+	return (1);
 }
 
 int	get_color(char *line, t_data *data)
 {
 	if (ft_strncmp(line, "F ", 2) == 0)
-		
+	line = NULL;
+	data = NULL;
+	return (1);
 }
 
 int	is_line_map(char *line)
 {
-	
+	line = NULL;
+	return (1);
 }
 
 int	check_description(char **map)
 {
-	
+	map = NULL;
+	return (1);
 }
 
 int	read_map_2(t_data *data)
 {
 	int		i;
+	char	*line;
+
 	i = 0;
+	line = NULL;
 	while (get_next_line(data->map_data->map_fd))
 	{
 		if (is_line_map(line))
@@ -64,7 +74,8 @@ int	read_map_2(t_data *data)
 		free (line);
 	}
 	if (check_description(data->map_data->map))
-		return ;
+		return (0);
+	return (1);
 }
 
 int	read_map_1(t_data *data)
@@ -73,6 +84,7 @@ int	read_map_1(t_data *data)
 	int		in_map;
 
 	in_map = 0;
+	line = NULL;
 	while (get_next_line(data->map_data->map_fd))
 	{
 		line = get_next_line(data->map_data->map_fd);
@@ -80,22 +92,24 @@ int	read_map_1(t_data *data)
 			|| ft_strncmp(line, "WE ", 3) || ft_strncmp(line, "EA ", 3) == 0)
 		{
 			if (!get_texture(line, data))
-				return ;
+				return (0);
 		}
 		if (ft_strncmp(line, "F ", 2) == 0 || ft_strncmp(line, "C ", 2) == 0)
 		{
 			if (get_color(line, data) == -1)
-				return ;
+				return (0);
 		}
 		free (line);
 	}
+	return (1);
 }
 
 int	parsing(t_data *data, char *filename)
 {
 	data->map_data->map_fd = open(filename, O_RDONLY);
 	if (data->map_data->map_fd == -1)
-		return ;
+		return (0);
 	read_map_1(data); // pour le texture et les couleur;
 	read_map_2(data); // pour la description;
+	return (1);
 }
--- srcs/init.c
+++ srcs/init.c
@@ -1,6 +1,6 @@
 #include "cub3d.h"
 
-int	init_data(t_data *data, char *filename)
+int	init_data(t_data *data)
 {
 	data->mlx_data->mlx = mlx_init(WIN_WIDTH, WIN_HEIGHT, "cub3d", true);
 	data->mlx_data->img = mlx_new_image(data->mlx_data->mlx, WIN_WIDTH,

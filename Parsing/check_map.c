0e3ce01629ef270ecf0dc0e3fcad1fc1fe7e7f10
Prprkitty <168284039+pepsicola7@users.noreply.github.com>
2025-03-06 14:34:27 +0100

    add the fonction of check_wall

--- Parsing/check_map.c
+++ Parsing/check_map.c
@@ -6,7 +6,7 @@
 /*   By: peli <peli@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/28 15:38:55 by peli              #+#    #+#             */
-/*   Updated: 2025/02/28 16:01:00 by peli             ###   ########.fr       */
+/*   Updated: 2025/03/06 14:28:23 by peli             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -114,6 +114,31 @@ int	check_line_vide(char **map)
 	return (1);
 }
 
+int	check_wall(char **map, int height)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (map[i])
+	{
+		j = 0;
+		if (i == 0 || i == height - 1)
+		{
+			while (map[i][j])
+			{
+				if (map[i][j] == '0' || map[i][j] == '0')
+					return (0);
+				j++;
+			}
+		}
+		else if (map[i][0] == '0' || map[i][ft_strlen(map[i]) - 1] == '0')
+				return (0);
+		i++;
+	}
+	return (1);
+}
+
 /*TODO: Modify the check_map function to check for the following:*/
 /*- Only check if the map is surrounded by walls (1), even if the player is surrounded by walls,*/
 /*- if a walkable path (0) exists next to a space or the end of the map, the map is invalid.*/
@@ -132,6 +157,12 @@ int	check_mur(t_data *data)
 	if (!check_flood(map, data->map_data->height))
 	{
 		printf("Error: Player is trapped!\n");
+		free_map(map);
+		return (0);
+	}
+	if (!check_wall(map, data->map_data->height ))
+	{
+		printf("Error: The wall is invalide!\n");
 		free_map(map);
 		return (0);
 	}
